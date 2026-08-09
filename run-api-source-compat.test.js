e01afd2181ee8095b279d60791b0bf870102997d
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-09 15:44:35 +0200

    Dek beide verplaatste API-bronpaden af in tests

--- run-api-source-compat.test.js
+++ run-api-source-compat.test.js
@@ -3,12 +3,14 @@
 const fs=require("fs");
 const path=require("path");
 const origineel=fs.readFileSync;
-const oudPad=path.resolve(__dirname,"api","waarschuwingen.js");
-const nieuwPad=path.resolve(__dirname,"lib","waarschuwingen.cjs");
+const redirects=new Map([
+  [path.resolve(__dirname,"api","waarschuwingen.js"),path.resolve(__dirname,"lib","waarschuwingen.cjs")],
+  [path.resolve(__dirname,"api","plaatsnaam.js"),path.resolve(__dirname,"lib","plaatsnaam.cjs")]
+]);
 
 fs.readFileSync=function(bestand,...args){
   const resolved=typeof bestand==="string"?path.resolve(bestand):bestand;
-  return origineel.call(this,resolved===oudPad?nieuwPad:bestand,...args);
+  return origineel.call(this,redirects.get(resolved)||bestand,...args);
 };
 
 require("./run.js");
