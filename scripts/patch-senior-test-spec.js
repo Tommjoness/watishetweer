5765b89b53b552c4f11317c706262dfded6f6867
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-09 16:25:32 +0200

    Werk architectuurtests bij naar canonieke bron

--- scripts/patch-senior-test-spec.js
+++ scripts/patch-senior-test-spec.js
@@ -1,11 +1,17 @@
 "use strict";
 const fs=require("fs"),path=require("path");
-const p=path.resolve(__dirname,"..","interpretatie-engine.test.js");
-let s=fs.readFileSync(p,"utf8");
-const oud='  assert(/hooguit enkele druppels/.test(zin),zin);';
-const nieuw='  assert(/geen meetbare hoeveelheid.*onzeker/.test(zin),zin);';
-const n=s.split(oud).length-1;
-if(n!==1)throw new Error("Verwacht exact één oude druppelclaim in de testspecificatie, gevonden "+n);
-s=s.replace(oud,nieuw);
-fs.writeFileSync(p,s,"utf8");
-console.log("Oude neerslagtestspecificatie gecorrigeerd naar onzeker modelsignaal.");
+const root=path.resolve(__dirname,"..");
+function vervang(p,oud,nieuw,label){
+  const f=path.join(root,p);let s=fs.readFileSync(f,"utf8");
+  const n=s.split(oud).length-1;if(n!==1)throw new Error(label+": verwacht één oude testspecificatie, gevonden "+n);
+  fs.writeFileSync(f,s.replace(oud,nieuw),"utf8");
+}
+vervang("interpretatie-engine.test.js",
+'  assert(/hooguit enkele druppels/.test(zin),zin);',
+'  assert(/geen meetbare hoeveelheid.*onzeker/.test(zin),zin);',
+"neerslagonzekerheid");
+vervang("built-production-regressions.test.js",
+'ok(/require\\("\\.\\/productie-hardening-v2\\.js"\\)/.test(build)&&/html=pasToe\\(html\\)/.test(build),"senior-hardening is onderdeel van één expliciete buildcompiler");',
+'ok(!/productie-hardening/.test(build)&&!/html=pasToe\\(html\\)/.test(build),"productsemantiek staat in canonieke bron en niet in build-hardening");',
+"canonieke bronarchitectuur");
+console.log("Verouderde testspecificaties bijgewerkt naar de nieuwe canonieke architectuur.");
