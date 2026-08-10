cbbdf9fd4ba2601bda6940cc3a4a0109255b973f
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-10 21:23:53 +0200

    Verwijder niet-gebruikte pollen-microcopytest

--- scripts/final-copy-polish.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const assert=require("assert");
-const c=require("./final-copy-polish.js");
-assert.equal(c.pollenKop("Pollen gras"),"Graspollen");
-assert.equal(c.pollenKop("Pollen bijvoet"),"Bijvoetpollen");
-assert.equal(c.pollenKop("Pollen berk"),"Berkenpollen");
-assert.equal(c.pollenKop("Pollen"),"Pollen");
-console.log("Finale Nederlandse microcopy: regressies geslaagd.");
