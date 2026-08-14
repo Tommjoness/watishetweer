918e8cda9b7ac82bf71ab35fac86501bb2cf5694
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-14 19:36:44 +0200

    SEO: centralize search metadata

---
+++ scripts/seo-foundation.config.js
@@ -0,0 +1,8 @@
+"use strict";
+
+module.exports=Object.freeze({
+  canonical:"https://watishetweer.nl/",
+  title:"Weer vandaag en 7-daagse verwachting | Wat is het weer?",
+  description:"Bekijk het actuele weer, neerslag voor de komende uren, de 7-daagse verwachting, luchtkwaliteit en nachtzicht voor plaatsen wereldwijd.",
+  siteName:"Wat is het weer?"
+});
