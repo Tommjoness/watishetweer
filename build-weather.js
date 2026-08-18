cc513266f149d78f3767818255e6befaf386f8e6
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-18 16:00:14 +0200

    Apply cache fallback land rule in base build

--- build-weather.js
+++ build-weather.js
@@ -63,6 +63,7 @@ function vervangProductregel(bron,productie,label){
 vervangProductregel(PRODUCT_CONFIG.EERSTE_BEZOEK_BRON,PRODUCT_CONFIG.EERSTE_BEZOEK_PRODUCTIE,"Eerste-bezoekblok");
 vervangProductregel(PRODUCT_CONFIG.KALENDERDAG_PUNTEN_BRON,PRODUCT_CONFIG.KALENDERDAG_PUNTEN_PRODUCTIE,"24-uursgrensregel");
 vervangProductregel(PRODUCT_CONFIG.OPHAALFOUT_BRON,PRODUCT_CONFIG.OPHAALFOUT_PRODUCTIE,"Ophaalfoutsemantiek");
+vervangProductregel(PRODUCT_CONFIG.CACHE_FALLBACK_LAND_BRON,PRODUCT_CONFIG.CACHE_FALLBACK_LAND_PRODUCTIE,"Cachefallback-landcontext");
 
 /* De compacte meelopende weerbalk gebruikte oorspronkelijk alleen een
    IntersectionObserver. Dat is zuinig, maar browsers mogen zo'n callback rond
