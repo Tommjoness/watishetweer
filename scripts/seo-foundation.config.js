0a4f92d04b38bcc0b6462c83fdc5a23f056d399b
Tommjoness <maitri.polwatteg@gmail.com>
2026-08-29 02:21:14 +0200

    Definieer vaste merkidentiteit voor SEO

--- scripts/seo-foundation.config.js
+++ scripts/seo-foundation.config.js
@@ -2,9 +2,17 @@
 
 module.exports=Object.freeze({
   canonical:"https://watishetweer.nl/",
-  title:"Weer vandaag en 7-daagse verwachting | Wat is het weer?",
-  description:"Bekijk het actuele weer, neerslag voor de komende uren, de 7-daagse verwachting, luchtkwaliteit en nachtzicht voor plaatsen wereldwijd.",
-  siteName:"Wat is het weer?",
+  title:"Weer vandaag en 7-daagse verwachting | watishetweer.nl",
+  description:"watishetweer.nl toont het actuele weer, neerslag voor de komende uren, de 7-daagse verwachting, luchtkwaliteit en nachtzicht voor plaatsen wereldwijd.",
+  siteName:"watishetweer.nl",
+  productName:"Wat is het weer?",
+  alternateNames:Object.freeze(["watishetweer","Wat is het weer?"]),
+  organizationId:"https://watishetweer.nl/#organization",
+  websiteId:"https://watishetweer.nl/#website",
+  logo:"https://watishetweer.nl/icon-512.png",
+  favicon:"https://watishetweer.nl/icon-192.png",
+  aboutUrl:"https://watishetweer.nl/over/",
+  brandDescription:"watishetweer.nl brengt actuele weersinformatie en modelverwachtingen voor plaatsen wereldwijd overzichtelijk samen.",
   googleVerification:"akmQYDWRuHI58wfAxGogZWggmZYSHagjnE5bDJkJ9RI",
   bingVerification:"850E623A7D1AB3BEF72DE662C74B5756"
 });
